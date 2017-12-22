f79c47abf46d368b5acaf924bef49163e34b12f2
Mario Guerriero <marioguerriero33@gmail.com>
2017-12-22 12:13:02 +0100

    fix footer styling and items ordering

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,25 +7,22 @@
   </div>
   <div class="footerColumn2">
     <h4> Help & Info </h4>
-    <div> <h5> Contact Us </h5> </div>
     <div> <h5> Shipping </h5> </div>
     <div> <h5> Payment </h5> </div>
+    <div> <h5> Size guide </h5> </div>
     <div> <h5> FAQ </h5> </div>
-    <div> <h5> Men's size guide </h5> </div>
-    <div> <h5> Women's size guide </h5> </div>
   </div>
   <div class="footerColumn3">
     <h4> About </h4>
     <div> <h5> About weAR </h5> </div>
     <div> <h5> Safety, Security and Privacy </h5> </div>
     <div> <h5> Terms & Conditions </h5> </div>
-    <div> <h5> Careers </h5> </div>
-    <div> <h5> Men's size guide </h5> </div>
-    <div> <h5> Women's size guide </h5> </div>
   </div>
   <div class="footerColumn4">
     <h4> Connect With Us </h4>
+    <div><a href="#" @click="newsletter_messagebox"><h5>Sign up for newsletter</h5></a></div>
     <div class="social_icons"></div>
+    <div> <h5> Careers </h5> </div>
   </div>
 </footer>
 </template>
@@ -37,7 +34,27 @@ export default {
     return {
       msg: 'Contact us'
     }
-  }
+  },
+  methods: {
+    newsletter_messagebox() {
+      this.$prompt('Please input your e-mail', 'Sign up for the newsletter', {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+        inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
+        inputErrorMessage: 'Invalid Email'
+      }).then(value => {
+        this.$message({
+          type: 'success',
+          message: 'Thanks for joining our community'
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: 'Input canceled'
+        });
+      });
+    }
+  } 
 }
 </script>
 
@@ -61,5 +78,9 @@ footer h4 {
 footer h5 {
   color: #909399;
   font-size: 80%
+}
+
+footer a {
+  text-decoration: none;
 }
 </style>
