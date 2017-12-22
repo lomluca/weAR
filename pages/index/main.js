fd983a93ea03b64b85b92288c274ee86fa2af863
Giuseppe Coccia <giuseppecoccia94@gmail.com>
2017-12-22 17:08:54 +0100

    Header fixed when scrolling

--- pages/index/main.js
+++ pages/index/main.js
@@ -9,3 +9,22 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+window.addEventListener('scroll', function(){  
+    var value = $(window).scrollTop();
+    if ( value > 200 ){
+        $("header").css("position", "fixed");
+        $("header").css("top", "0px");
+        $("header").css("z-index", "9999");
+        $(".headerRow1").css("display", "none");
+        $(".headerRow2").css("display", "none");
+        $("main").css("margin-top", "120px");
+        $("main").css("padding-top", "30px");
+    }
+    else{
+        $("header").css({ 'position' : '' , 'top' : '' , 'z-index' : '' });
+        $(".headerRow1").css("display", "inline-block");
+        $(".headerRow2").css("display", "inline-block");
+        $("main").css({ 'margin-top' : '', 'padding-top' : '' });
+    }
+}, true)
