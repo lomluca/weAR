6aa6a4e7992c26fdb42fc27ffe183f43c53edfe5
Paolo Moriello <paolomoriello94@gmail.com>
2017-12-24 23:15:56 +0100

    changed main_app structure: added main component, and removed code from App.vue; added imageBlock below carosel

---
+++ components/Main.vue
@@ -0,0 +1,99 @@
+<template>
+  <section>
+    <!-- Carousel -->
+    <div class="carouselBlock">
+      <el-carousel indicator-position="outside">
+        <el-carousel-item
+          v-for="item in carouselData"
+          :item="item"
+          :key="item.id" >
+          <a :href="item.href"><img :src="item.asset" :alt="item.alt"></a>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+
+    <!-- Other images -->
+    <div class="imageBlock">
+      <div class="blockRow">
+        <div class="blockCol">
+          <div class="imageContainer">
+            <a href="#"><img src="/assets/carousel/1.jpg" alt=""></a>
+          </div>
+        </div>
+        <div class="blockCol">
+          <div class="description">
+            Prova
+          </div>
+        </div>
+      </div>
+
+      <div class="blockRow">
+        <div class="blockCol">
+          <div class="description">
+            Prova
+          </div>
+        </div>
+        <div class="blockCol">
+          <div class="imageContainer">
+            <a href="#"><img src="/assets/carousel/2.jpg" alt=""></a>
+          </div>
+        </div>
+      </div>
+
+      <div class="blockRow">
+        <div class="blockCol">
+          <div class="imageContainer">
+            <a href="#"><img src="/assets/carousel/1.jpg" alt=""></a>
+          </div>
+        </div>
+        <div class="blockCol">
+          <div class="description">
+            Prova
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'wear-main',
+  data () {
+    return {
+      carouselData: getCarouselData()['data'] // Defined in public/js/model.js
+    }
+  }
+}
+</script>
+
+<style>
+.el-carousel__item a {
+  width: 100%;
+  height: 100%;
+}
+
+.el-carousel__item img {
+  width: 100%;
+  height: 100%;
+}
+
+.carouselBlock {
+  margin-bottom: 30px;
+}
+
+.imageBlock .blockRow {
+  width: 100%;
+  height: auto;
+  display: inline-block;
+  vertical-align: top;
+}
+.imageBlock .blockRow .blockCol {
+  width: 50%;
+  display: inline-block;
+  float: left
+}
+.imageBlock .blockRow .blockCol .imageContainer {
+  overflow: hidden;
+}
+</style>
--- pages/index/App.vue
+++ pages/index/App.vue
@@ -6,20 +6,8 @@
   </el-header>
 
   <!-- Page's main content -->
-  <el-main style="overflow: initial">
-    <!-- Carousel -->
-    <div class="block">
-      <el-carousel indicator-position="outside">
-        <el-carousel-item
-          v-for="item in carouselData"
-          :item="item"
-          :key="item.id" >
-          <a :href="item.href">
-            <img :src="item.asset" :alt="item.alt">
-          </a>
-        </el-carousel-item>
-      </el-carousel>
-    </div>
+  <el-main>
+    <wear-main />
   </el-main>
 
   <!-- Footer -->
@@ -33,17 +21,14 @@
 import WearHeader from '../../components/Header'
 import WearFooter from '../../components/Footer'
 import WearNav from '../../components/Nav'
+import WearMain from '../../components/Main'
 
 export default {
   name: 'app',
-  data () {
-    return {
-      carouselData: getCarouselData()['data'] // Defined in public/js/model.js
-    }
-  },
   components: {
     'wear-header': WearHeader,
     'wear-nav': WearNav,
+    'wear-main': WearMain,
     'wear-footer': WearFooter
   }
 }
@@ -55,17 +40,8 @@ export default {
   padding: 0px;
 }
 
-.el-main{
-  min-height: 100%;
-}
-
-.el-carousel__item a {
-  width: 100%;
-  height: 100%;
-}
-
-.el-carousel__item img {
-  width: 100%;
-  height: 100%;
+.el-main {
+  height: auto;
+  overflow: initial;
 }
 </style>
