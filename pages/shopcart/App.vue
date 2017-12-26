22ae54cfccdc1de0a88a97b78d171b000cd08ebd
Paolo Moriello <paolomoriello94@gmail.com>
2017-12-26 18:19:49 +0100

    implemented shop cart base page

--- pages/shopcart/App.vue
+++ pages/shopcart/App.vue
@@ -5,10 +5,49 @@
       <wear-header />
     </el-header>
 
-
     <!-- Main content -->
-    <el-main style="overflow: initial">
+    <el-main>
+      <div class="cartLeftWrapper">
+        <el-table
+          :data="shopcartData">
+          <el-table-column
+            label="Item">
+            <template slot-scope="scope">
+              <div class="shopRowWrapper">
+                <img width="70px" height="70px" :src="scope.row.asset" :alt="scope.row.alt" :href="scope.row.href"/>
+                <span style="margin-left: 10px">{{ scope.row.name }}</span>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="Price">
+            <template slot-scope="scope">
+              <div class="shopRowWrapper">
+                <span>{{ scope.row.price }}</span>
+                <i>€</i>
+              </div>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="Quantity">
+            <template slot-scope="scope">
+              <div class="shopRowWrapper">
+                <el-input-number v-model="scope.row.quantity"></el-input-number>
+              </div>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="shopCartTableFooter">
+          <h1>Total: x</h1>
+          <el-button>Checkout</el-button>
+        </div>
+      </div>
 
+      <div class="cartRightWrapper">
+        <h1>Delivery Address:</h1>
+        <p>some address</p>
+        <el-button>Edit</el-button>
+      </div>
     </el-main>
 
     <!-- Footer -->
@@ -26,7 +65,16 @@ export default {
   name: 'app',
   data: function() {
     return {
-      shopcart: getShopCartData()
+      //!!!!!! non funziona richiamando la funzione da model, non capisco perché
+      shopcartData: [
+        { id: 1, name: 'name', asset: '/assets/carousel/1.jpg', price:'10', quantity:'1', alt: 'Ad Banner 1', href: '#'  },
+        { id: 2, name: 'name', asset: '/assets/carousel/2.jpg', price:'3', quantity:'3', alt: 'Ad Banner 2', href: '#'  },
+        { id: 3, name: 'name', asset: '/assets/carousel/3.jpg', price:'4', quantity:'1', alt: 'Ad Banner 3', href: '#'  },
+        { id: 4, name: 'name', asset: '/assets/carousel/4.jpg', price:'5', quantity:'2', alt: 'Ad Banner 4', href: '#'  },
+        { id: 4, name: 'name', asset: '/assets/carousel/4.jpg', price:'5', quantity:'2', alt: 'Ad Banner 4', href: '#'  },
+        { id: 4, name: 'name', asset: '/assets/carousel/4.jpg', price:'5', quantity:'2', alt: 'Ad Banner 4', href: '#'  },
+        { id: 4, name: 'name', asset: '/assets/carousel/4.jpg', price:'5', quantity:'2', alt: 'Ad Banner 4', href: '#'  }
+      ]
     }
   },
   components: {
@@ -41,8 +89,31 @@ export default {
   box-sizing: content-box;
   padding: 0px;
 }
-.list-item-image {
-  width: 25%;
-  height: 10%;
+.el-footer{
+  box-sizing: content-box;
+  padding: 0px;
+}
+.el-main {
+  overflow: initial;
+  box-sizing: content-box;
+  padding: 0px;
+  height: auto;
+}
+
+.el-main .cartLeftWrapper {
+  width: 70%;
+  display: inline-block;
+  height: auto;
+  float: left
+}
+.el-main .cartRightWrapper {
+  display: inline-block;
+}
+
+.el-main .cartLeftWrapper .shopRowWrapper {
+  float: left;
+}
+.el-main .shopCartTableFooter {
+  float: right;
 }
 </style>
