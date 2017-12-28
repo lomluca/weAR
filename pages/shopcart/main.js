492e3c8877d77c664e4bceca07d1559b378206ae
giuseppeCoccia <giuseppecoccia94@gmail.com>
2017-12-28 16:33:31 +0100

    Update main.js

--- pages/shopcart/main.js
+++ pages/shopcart/main.js
@@ -2,11 +2,8 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
-import Vuetify from 'vuetify'
-import '../../node_modules/vuetify/dist/vuetify.min.css'
  
 Vue.use(ElementUI)
-Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
