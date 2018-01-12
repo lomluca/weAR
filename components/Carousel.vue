c0f6a231b45f62dc5a34b8076cfacf309f50470f
Paolo Moriello <paolomoriello94@gmail.com>
2018-01-12 15:44:13 +0100

    added more text in the home page

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -6,6 +6,7 @@
     :key="item.id" >
       <a :href="item.href"><img :src="item.asset" :alt="item.alt"></a>
       <span class="text_over_image">SALE</span>
+      <span class="text_over_image_below">TRY YOUR STYLE NOW</span>
   </el-carousel-item>
 </el-carousel>
 </template>
@@ -16,14 +17,22 @@ export default {
   props: ['items'],
   data() {
     return {
+      windowHeight: window.innerHeight,
+      windowWidth: window.innerWidth,
       imageHeight: (window.innerHeight*0.9)+'px'
+    }
+  },
+  watch: {
+    windowWidth(newWidth, oldWidth) {
+      this.imageHeight = this.windowHeight;
     }
   },
   mounted() {
     let that = this;
     this.$nextTick(function() {
       window.addEventListener('resize', function(e) {
-        that.imageHeight = (window.innerHeight*0.9)+'px';
+        this.windowHeight = window.innerHeight;
+        this.windowWidth = window.innerWidth;
       });
     })
   }
@@ -59,6 +68,21 @@ export default {
       font-weight: bold;
       text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
   }
+
+  .text_over_image_below {
+      position: absolute;
+      margin: auto;
+      top: 55%;
+      bottom: 45%;
+      right: 50%;
+      left: 47%;
+      transform: translate(-50%, -50%);
+
+      color:#fff;
+      font-size: 2em;
+      font-weight: bold;
+      text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
+  }
 }
 
 .text_over_image {
@@ -69,8 +93,22 @@ export default {
     transform: translate(-50%, -50%);
 
     color:#fff;
-    font-size: 4em;
+    font-size: 3.8em;
     font-weight: bold;
     text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
 }
+
+.text_over_image_below {
+    position: absolute;
+    margin: auto;
+    top: 57%;
+    bottom: 43%;
+    transform: translate(-50%, -50%);
+
+    color:#fff;
+    font-size: 1.5em;
+    font-weight: bold;
+    text-shadow: 1px 0 0 #000, 0 -1px 0 #000, 0 1px 0 #000, -1px 0 0 #000;
+}
+
 </style>
