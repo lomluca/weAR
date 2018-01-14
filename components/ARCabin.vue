c9281d9179de514f47ed2f63eef0fb338879b98d
Erika <Erika>
2018-01-14 11:14:11 +0100

    Video streaming fixed

--- components/ARCabin.vue
+++ components/ARCabin.vue
@@ -25,10 +25,36 @@ export default {
     var constraints = { audio: false, video: true };
 
     var handleSuccess = function (stream) {  
+        var videoTracks = stream.getVideoTracks();
+        console.log('Got stream with constraints:', constraints);
+        console.log('Using video device: ' + videoTracks[0].label);
+        stream.oninactive = function() {
+          console.log('Stream inactive');
+        };
+        window.stream = stream; // make variable available to browser console
         video.srcObject = stream;
     };
 
-    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess);
+    var errorMsg = function(msg, error) {
+      errorElement.innerHTML += '<p>' + msg + '</p>';
+      if (typeof error !== 'undefined') {
+          console.error(error);
+      }
+    }
+
+    var handleError = function(error) {
+      if (error.name === 'ConstraintNotSatisfiedError') {
+        errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
+            constraints.video.width.exact + ' px is not supported by your device.');
+      } else if (error.name === 'PermissionDeniedError') {
+        errorMsg('Permissions have not been granted to use your camera and ' +
+          'microphone, you need to allow the page access to your devices in ' +
+          'order for the demo to work.');
+      }
+      errorMsg('getUserMedia error: ' + error.name, error);
+    }
+
+    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
   }
 }
 </script>
