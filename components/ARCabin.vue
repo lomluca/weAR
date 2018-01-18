4666bbcf4bc889d619c50ccd952477e0e78a0507
Luca <lomluca12@gmail.com>
2018-01-18 23:41:23 +0100

    Getusermedia fix2 on Safari

--- components/ARCabin.vue
+++ components/ARCabin.vue
@@ -8,6 +8,43 @@
 </template>
 
 <script>
+
+var video = document.querySelector("#ar-component");
+     
+var constraints = window.constraints = { audio: false, video: true };
+
+var handleSuccess = function (stream) {  
+    var videoTracks = stream.getVideoTracks();
+    console.log('Got stream with constraints:', constraints);
+    console.log('Using video device: ' + videoTracks[0].label);
+    stream.oninactive = function() {
+      console.log('Stream inactive');
+    };
+    window.stream = stream; // make variable available to browser console
+    video.srcObject = stream;
+};
+
+var errorMsg = function(msg, error) {
+  errorElement.innerHTML += '<p>' + msg + '</p>';
+  if (typeof error !== 'undefined') {
+      console.error(error);
+  }
+}
+
+var handleError = function(error) {
+  if (error.name === 'ConstraintNotSatisfiedError') {
+    errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
+        constraints.video.width.exact + ' px is not supported by your device.');
+  } else if (error.name === 'PermissionDeniedError') {
+    errorMsg('Permissions have not been granted to use your camera and ' +
+      'microphone, you need to allow the page access to your devices in ' +
+      'order for the demo to work.');
+  }
+  errorMsg('getUserMedia error: ' + error.name, error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+
 export default {
   name: 'wear-arcabin',
   props: [ 'item' ],
@@ -17,44 +54,6 @@ export default {
   },
   methods: {
     
-  },
-  // Request for webcam access as soon as the component is mounted
-  mounted() {
-    var video = document.querySelector("#ar-component");
-     
-    var constraints = window.constraints = { audio: false, video: true };
-
-    var handleSuccess = function (stream) {  
-        var videoTracks = stream.getVideoTracks();
-        console.log('Got stream with constraints:', constraints);
-        console.log('Using video device: ' + videoTracks[0].label);
-        stream.oninactive = function() {
-          console.log('Stream inactive');
-        };
-        window.stream = stream; // make variable available to browser console
-        video.srcObject = stream;
-    };
-
-    var errorMsg = function(msg, error) {
-      errorElement.innerHTML += '<p>' + msg + '</p>';
-      if (typeof error !== 'undefined') {
-          console.error(error);
-      }
-    }
-
-    var handleError = function(error) {
-      if (error.name === 'ConstraintNotSatisfiedError') {
-        errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
-            constraints.video.width.exact + ' px is not supported by your device.');
-      } else if (error.name === 'PermissionDeniedError') {
-        errorMsg('Permissions have not been granted to use your camera and ' +
-          'microphone, you need to allow the page access to your devices in ' +
-          'order for the demo to work.');
-      }
-      errorMsg('getUserMedia error: ' + error.name, error);
-    }
-
-    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
   }
 }
 </script>
