7d4c574dbd8c88b0c7da72b0cc92a81e9fc3f203
Luca <lomluca12@gmail.com>
2018-01-19 22:08:39 +0100

    Implementing profile picture

--- pages/profile/App.vue
+++ pages/profile/App.vue
@@ -13,9 +13,10 @@
           <!--<img class="profile-img":src="sourceImg" v-if="showImg"></img>
           <input type="file" accept="image/*"@change="changePic($event)"></input>-->
           <video v-show="cameraShowed" playsinline="true" autoplay="true" id="video-box"></video>
-          <canvas v-show="cameraShowed" id="snapshot" width="100" height="100"></canvas>
+          <canvas v-show="previewShowed" id="snapshot" width="100" height="100"></canvas>
           <img src="" alt="Profile picture" id="profile-pic">
-          <el-button type="primary" @click="takePhoto">Take photo</el-button>
+          <el-button type="primary" @click="takePhoto">Change photo</el-button>
+          <el-button type="primary" @click="takeSnap">Take photo</el-button>
           <ul class="list-info">
             <li><span class="ligth-text">fullname</span> <span class="bold-text">{{ fullname }}</span></li>
             <li><span class="ligth-text">email</span> <span class="bold-text">{{ mail }}</span></li>
@@ -172,6 +173,8 @@ export default {
       canvas = document.getElementById("snapshot");
       ctx = canvas.getContext('2d');
       ctx.drawImage(video, 0,0, canvas.width, canvas.height);
+      cameraShowed = false;
+      previewShowed = true;
     }
   },
   beforeMount() {
@@ -227,8 +230,8 @@ export default {
   }
 
   #video-box {
-    width: 50px;
-    height: 50px;
+    width: 150px;
+    height: 150px;
   }
 
 </style>
